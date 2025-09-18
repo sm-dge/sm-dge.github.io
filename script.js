3ed8acf615674c9eeeacd2d96f08c1c79751a4b6
James <j4m3sgs@gmail.com>
2025-09-18 15:23:23 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
 function darkToggle() {
   var element = document.body;
   element.classList.toggle("dark-mode");
-}
-
-var img = document.getElementById('github-logo');
-if (document.body.classList.contains('dark-mode')) {
-  img.src = 'github_logo_dark.png';
-} else {
-  img.src = 'github_logo_light.png';
+  var img = document.getElementById('github-logo');
+  if (document.body.classList.contains('dark-mode')) {
+    img.src = 'github_logo_dark.png';
+  } else {
+    img.src = 'github_logo_light.png';
+  }
 }
