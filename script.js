44be8184f668d416105b315461d9339f1db6ac3a
James <j4m3sgs@gmail.com>
2025-09-18 15:32:56 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,4 @@
 function darkToggle() {
   var element = document.body;
   element.classList.toggle("dark-mode");
-  var img = document.getElementById("github-logo");
-  if (document.body.classList.contains("dark-mode")) {
-    img.src = "github_logo_dark.png";
-  } else {
-    img.src = "github_logo_light.png";
-  }
 }
